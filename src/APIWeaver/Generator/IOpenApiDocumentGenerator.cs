184149e10857c68c938ef9bd4e90ea8223640f21
Justin Lampe <xC0dex@users.noreply.github.com>
2024-01-22 21:29:34 +0100

    feat: Add NuGet properties (#4)

---
+++ src/APIWeaver/Generator/IOpenApiDocumentGenerator.cs
@@ -0,0 +1,15 @@
+using Microsoft.OpenApi.Models;
+
+namespace APIWeaver.Generator;
+
+
+/// <summary>
+/// Interface for generating OpenAPI documents.
+/// </summary>
+public interface IOpenApiDocumentGenerator
+{
+    /// <summary>
+    /// Asynchronously generates an OpenAPI document.
+    /// </summary>
+    Task<OpenApiDocument> GenerateDocumentAsync();
+}
