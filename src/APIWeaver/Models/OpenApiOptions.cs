4492f046d479a2884a9714bae8b4ed1b4670aaac
xC0dex <xC0dex@users.noreply.github.com>
2024-01-26 21:38:13 +0100

    feat: Update

--- src/APIWeaver/Models/OpenApiOptions.cs
+++ src/APIWeaver/Models/OpenApiOptions.cs
@@ -7,7 +7,6 @@ namespace APIWeaver.Models;
 /// </summary>
 public sealed class OpenApiOptions
 {
-
     /// <summary>
     /// Gets or sets the collection of OpenAPI documents in the application.
     /// Each key-value pair represents a unique OpenAPI document, where the key is the document name and the value is the OpenAPI information.
