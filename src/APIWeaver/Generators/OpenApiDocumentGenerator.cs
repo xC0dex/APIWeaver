33f3cc6abf665f56132b1bb0125b3973982e64d4
xC0dex <xC0dex@users.noreply.github.com>
2024-01-28 11:22:05 +0100

    feat: Update

--- src/APIWeaver/Generators/OpenApiDocumentGenerator.cs
+++ src/APIWeaver/Generators/OpenApiDocumentGenerator.cs
@@ -1,4 +1,3 @@
-using APIWeaver.Exceptions;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.OpenApi.Models;
 
@@ -8,28 +7,11 @@ internal sealed class OpenApiDocumentGenerator(IApiDescriptionGroupCollectionPro
 {
     public Task<OpenApiDocument> GenerateDocumentAsync(string documentName, OpenApiInfo openApiInfo, CancellationToken cancellationToken = default)
     {
-        var apiDescriptions = apiDescriptionProvider.ApiDescriptionGroups.Items.SelectMany(x => x.Items).ToArray();
-        var paths = GeneratePaths(apiDescriptions);
         var document = new OpenApiDocument
         {
             Info = openApiInfo,
-            Components = new OpenApiComponents(),
-            Paths = paths,
-            Servers = new List<OpenApiServer>(),
-            SecurityRequirements = new List<OpenApiSecurityRequirement>()
+            Paths = new OpenApiPaths()
         };
         return Task.FromResult(document);
-    }
-
-    private OpenApiPaths GeneratePaths(IEnumerable<ApiDescription> apiDescriptions)
-    {
-        var paths = new OpenApiPaths();
-        foreach (var apiDescription in apiDescriptions)
-        {
-            var metadata = apiDescription.ActionDescriptor.EndpointMetadata;
-            var operation = metadata.OfType<OpenApiOperation>().FirstOrDefault() ?? throw new OpenApiOperationNotFoundException(apiDescription.RelativePath!);
-        }
-
-        return paths;
     }
 }
