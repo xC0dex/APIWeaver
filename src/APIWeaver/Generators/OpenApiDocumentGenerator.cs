248a83e7a17c90e6a279a61d307cf3f96619a981
xC0dex <xC0dex@users.noreply.github.com>
2024-01-28 11:25:49 +0100

    feat: Update

--- src/APIWeaver/Generators/OpenApiDocumentGenerator.cs
+++ src/APIWeaver/Generators/OpenApiDocumentGenerator.cs
@@ -1,9 +1,8 @@
-using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.OpenApi.Models;
 
 namespace APIWeaver.Generators;
 
-internal sealed class OpenApiDocumentGenerator(IApiDescriptionGroupCollectionProvider apiDescriptionProvider) : IOpenApiDocumentGenerator
+internal sealed class OpenApiDocumentGenerator() : IOpenApiDocumentGenerator
 {
     public Task<OpenApiDocument> GenerateDocumentAsync(string documentName, OpenApiInfo openApiInfo, CancellationToken cancellationToken = default)
     {
