f2313c890bb5bba2651e827c8d90b52fdc0ba209
Justin Lampe <xC0dex@users.noreply.github.com>
2024-08-24 21:08:02 +0200

    feat: Start support

--- src/APIWeaver.Swagger/Constants.cs
+++ src/APIWeaver.Swagger/Constants.cs
@@ -2,15 +2,9 @@ namespace APIWeaver.Swagger;
 
 internal struct Constants
 {
-    /// <summary>
-    /// The name of the initial document.
-    /// </summary>
-    public const string InitialDocumentName = "v1";
+    internal const string InitialDocumentName = "v1";
 
-    /// <summary>
-    /// The default prefix for endpoints.
-    /// </summary>
-    public const string DefaultSwaggerRoutePrefix = "swagger";
+    internal const string DefaultSwaggerRoutePrefix = "swagger";
 
-    public const string DefaultOpenApiRoutePattern = "/openapi/{documentName}.json";
+    internal const string DefaultOpenApiRoutePattern = "/openapi/{documentName}.json";
 }
