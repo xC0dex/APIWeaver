a729ecd1023e51d25ad79426e9db79481768f5da
Justin Lampe <xC0dex@users.noreply.github.com>
2024-08-25 19:00:07 +0200

    feat: Update

--- tests/APIWeaver.Swagger.Tests/SwaggerConfigurationMiddlewareTests.cs
+++ tests/APIWeaver.Swagger.Tests/SwaggerConfigurationMiddlewareTests.cs
@@ -102,40 +102,7 @@ public sealed class SwaggerConfigurationMiddlewareTests : IClassFixture<WebAppli
                                 """;
         content.ReplaceLineEndings().Should().Be(expected);
     }
-
-    [Fact]
-    public async Task Middleware_ShouldReturnJs_WhenRequested()
-    {
-        // Act
-        var response = await _client.GetAsync("/swagger/swagger-initializer.js");
-
-        // Assert
-        response.StatusCode.Should().Be(HttpStatusCode.OK);
-        var content = await response.Content.ReadAsStringAsync();
-
-        const string expected = """
-                                fetch('./configuration.json')
-                                    .then(response => response.json())
-                                    .then(({title, uiOptions, additionalUiOptions, oAuth2Options}) => {
-                                        document.title = title;
-                                        if (additionalUiOptions.darkMode) {
-                                            appendHeaderContent('<link rel="stylesheet" type="text/css" href="./dark-mode.css" />');
-                                        }
-                                        uiOptions.dom_id = '#swagger-ui';
-                                        uiOptions.presets = [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset];
-                                        uiOptions.plugins = [SwaggerUIBundle.plugins.DownloadUrl];
-                                        uiOptions.layout = 'StandaloneLayout';
-                                        window.ui = SwaggerUIBundle(uiOptions);
-                                        oAuth2Options && window.ui.initOAuth(oAuth2Options);
-                                    });
-
-                                function appendHeaderContent(content) {
-                                    document.getElementsByTagName('head')[0].insertAdjacentHTML('beforeend', content);
-                                }
-                                """;
-        content.ReplaceLineEndings().Should().Be(expected);
-    }
-
+    
     [Fact]
     public async Task Middleware_ShouldReturnValidConfiguration_WhenRequested()
     {
