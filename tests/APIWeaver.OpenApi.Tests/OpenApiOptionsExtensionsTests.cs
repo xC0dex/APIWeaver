0b3bca83c54701a846f40ff8ac40cc859b8549e2
Justin Lampe <xC0dex@users.noreply.github.com>
2024-08-29 19:56:52 +0200

    test: Increase coverage (#95)

---
+++ tests/APIWeaver.OpenApi.Tests/OpenApiOptionsExtensionsTests.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Models;
+using NSubstitute;
+
+namespace APIWeaver.OpenApi.Tests;
+
+public class OpenApiOptionsExtensionsTests
+{
+    [Fact]
+    public async Task AddDocumentTransformer_ShouldAddTransformer()
+    {
+        // Arrange
+        var options = new OpenApiOptions();
+        var openApiDocument = new OpenApiDocument();
+        var transformerContext = new OpenApiDocumentTransformerContext
+        {
+            ApplicationServices = Substitute.For<IServiceProvider>(),
+            DescriptionGroups = [],
+            DocumentName = "v1"
+        };
+
+        // Act
+        options.AddDocumentTransformer((document, _) =>
+        {
+            document.Info = new OpenApiInfo
+            {
+                Title = "v1"
+            };
+        });
+
+        // Assert
+        var documentTransformers = OpenApiOptionsAccessor.GetDocumentTransformers(options);
+        documentTransformers.Should().HaveCount(1);
+        await documentTransformers[0].TransformAsync(openApiDocument, transformerContext, CancellationToken.None);
+        openApiDocument.Info.Title.Should().Be("v1");
+    }
+
+    [Fact]
+    public async Task AddOperationTransformer_Overload_ShouldAddTransformer()
+    {
+        // Arrange
+        var options = new OpenApiOptions();
+        var openApiOperation = new OpenApiOperation();
+        var transformerContext = new OpenApiOperationTransformerContext
+        {
+            ApplicationServices = Substitute.For<IServiceProvider>(),
+            DocumentName = "v1",
+            Description = new ApiDescription()
+        };
+
+        // Act
+        options.AddOperationTransformer((operation, _) => { operation.OperationId = "test"; });
+
+        // Assert
+        var operationTransformers = OpenApiOptionsAccessor.GetOperationTransformers(options);
+        operationTransformers.Should().HaveCount(1);
+        await operationTransformers[0].TransformAsync(openApiOperation, transformerContext, CancellationToken.None);
+        openApiOperation.OperationId.Should().Be("test");
+    }
+}
