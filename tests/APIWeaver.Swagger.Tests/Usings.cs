70a38c74b6d73518c1bfa6a1e1e8f2c48c2856ab
Justin Lampe <xC0dex@users.noreply.github.com>
2024-10-28 14:54:35 +0100

    chore: Cleanup

--- tests/APIWeaver.Swagger.Tests/Usings.cs
+++
@@ -1,8 +0,0 @@
-global using Xunit;
-global using System.Net;
-global using System.Text.Json;
-global using NSubstitute;
-global using FluentAssertions;
-global using Microsoft.AspNetCore.Mvc.Testing;
-global using Microsoft.AspNetCore.TestHost;
-global using Microsoft.Extensions.DependencyInjection;
